a544c22417330e645b258e8331e467d2bcf1725c
BDXGaming <jx.welsko@gmail.com>
2022-11-02 13:39:45 -0400

    Remove ununsed files

--- utils/transpiler.py
+++
@@ -1,7 +0,0 @@
-STRING_CONTAINERS = ["'", '"']
-STRING_FORMATTING_ERROR = "String formatting error"
-JLANG_KEYWORDS = ["if", "elif", "else","print", "for", "while"]
-TYPE_KEYWORDS = ['string', "num", "int", "list", "hashmap", "dict"]
-JLANG_SIGNS = ["{", "}"]
-
-variables = {}
